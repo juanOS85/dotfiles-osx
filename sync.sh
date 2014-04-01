264169203ef288d36789d4fed47e3f46c31f7be3
Juan Obando <juanchopx2@gmail.com>
2014-03-31 22:52:28 -0500

    Sync routine.

---
+++ sync.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+# get latest changes from repo
+git pull
+
+function doIt() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh" -av . ~
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then # override changes without warning
+  doIt
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+  fi
+fi
+
+unset doIt
+
+source ~/.bash_profile
