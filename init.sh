b35003658ef3062d4fd6206295f29310dd6fe547
Juan Obando <juanchopx2@gmail.com>
2014-10-26 09:24:16 -0500

    Commented out not installing well formulas. Add draw_white_space config for ST.

--- init.sh
+++ init.sh
@@ -12,10 +12,7 @@ brew update
 # General dependencies
 brew install pkg-config
 brew install makedepend
-
 brew install openssl
-brew link --force openssl
-
 brew install libmetalink
 brew install libssh2
 brew install libidn
@@ -48,19 +45,15 @@ brew install go
 # Vim optional dependecy
 brew install lua --with-completion
 
-brew install wget --enable-iri
-
-brew install python --with-brewed-tk
+brew install wget --with-iri
 
 ### Development
 brew install mariadb
 brew install sqlite --with-functions --with-icu4c
 brew install postgres --enable-dtrace --with-python
 
-# VCS
-brew install subversion --with-python
-#brew install git --with-blk-sha1 --with-brewed-curl --with-brewed-openssl --with-brewed-svn --with-gettext --with-pcre --with-persistent-https
-brew install git --with-blk-sha1 --with-brewed-svn --with-gettext --with-pcre --with-persistent-https
+# Python
+brew install python --with-brewed-tk
 
 # PHP dependencies
 brew install libpng
@@ -77,7 +70,14 @@ brew install flex
 
 # PHP
 #brew install php56 --with-gmp --with-homebrew-curl --with-homebrew-libxslt --with-homebrew-openssl --with-pgsql --with-phpdbg --with-tidy --without-snmp
+# Failing in Yosemite
 brew install php56 --with-gmp --with-homebrew-libxslt --with-homebrew-openssl --with-pgsql --with-tidy --without-snmp
+
+# VCS
+# Failing in Yosemite
+brew install subversion --with-python
+#brew install git --with-blk-sha1 --with-brewed-curl --with-brewed-openssl --with-brewed-svn --with-gettext --with-pcre --with-persistent-https
+brew install git --with-blk-sha1 --with-gettext --with-pcre --with-persistent-https
 
 # NginX
 # for install it with Fushion Passenger support
