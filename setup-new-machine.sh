7c7c5cc727b709305ed731fc02243ff27e7fae8b
Juan Obando <juanchopx2@gmail.com>
2019-07-07 20:53:24 -0500

    Update initial setup script

--- setup-new-machine.sh
+++ setup-new-machine.sh
@@ -2,7 +2,7 @@
 # Install if it isn't, yet
 if test ! $(which brew); then
   echo "Installing Homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Install bundle command
@@ -13,10 +13,11 @@ brew bundle
 
 # Install NVM
 echo "Installing NVM..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-# Set ZSH as default shell
-chsh -s /bin/zsh
+
+# INstall Oh-My-ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # Remove unused Bash files
 rm -frv $HOME/.bash_* $HOME/.bashrc $HOME/.profile
